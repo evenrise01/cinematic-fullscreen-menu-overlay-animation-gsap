a3e1464f409730bd06252b4976474d384f5c91d4
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-10-25 15:01:33 +0530

    HTML setup for the animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,98 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <nav>
+        <div className="logo">
+          <a href="#">Evenrise</a>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="menu-toggle">
+          <p id="menu-open">Menu</p>
+          <p id="menu-open">Close</p>
         </div>
-      </main>
-    </div>
+      </nav>
+      {/* Clip path to the the menu-overlay and tilt animation to the menu-content*/}
+      <div className="menu-overlay">
+        <div className="menu-content">
+          <div className="menu-items">
+            <div className="col-lg">
+              <div className="menu-preview-img">
+                <img src="/img-1.jpg" alt="" />
+              </div>
+            </div>
+            <div className="col-sm">
+              <div className="menu-links">
+                <div className="link">
+                  <a href="#" data-img="/img-1.jpg">
+                    Visions
+                  </a>
+                </div>
+
+                <div className="link">
+                  <a href="#" data-img="/img-2.jpg">
+                    Core
+                  </a>
+                </div>
+
+                <div className="link">
+                  <a href="#" data-img="/img-3.jpg">
+                    Work
+                  </a>
+                </div>
+
+                <div className="link">
+                  <a href="#" data-img="/img-4.jpg">
+                    Connect
+                  </a>
+                </div>
+              </div>
+              <div className="menu-socials">
+                <div className="social">
+                  <a href="#" data-img="/img-1.jpg">
+                    Behance
+                  </a>
+                </div>
+
+                <div className="social">
+                  <a href="#" data-img="/img-2.jpg">
+                    Dribble
+                  </a>
+                </div>
+
+                <div className="social">
+                  <a href="#" data-img="/img-3.jpg">
+                    Instagram
+                  </a>
+                </div>
+
+                <div className="social">
+                  <a href="#" data-img="/img-4.jpg">
+                    LinkedIn
+                  </a>
+                </div>
+              </div>
+              <div className="menu-footer">
+                <div className="col-lg">
+                  <a href="#">Run Sequence</a>
+                </div>
+                <div className="col-sm">
+                  <a href="#">Origin</a>
+                  <a href="#">Know More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <section className="hero">
+          <div className="hero-img">
+            <img src="/hero.jpg" alt="" />
+          </div>
+          <h1>Elevate your websites with Intention.</h1>
+        </section>
+      </div>
+    </>
   );
 }
