a487e54f9888c8f4359ef6b5a852b02a389a3ea5
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-10-26 22:10:38 +0530

    complete gsap animation for the menu overaly

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,191 @@
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import Image from "next/image";
+
 export default function Home() {
+  useGSAP(() => {
+    // DOM element selectors
+    const container = document.querySelector(".container");
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menuOverlay = document.querySelector(".menu-overlay");
+    const menuContent = document.querySelector(".menu-content");
+    const menuPreviewImg = document.querySelector(".menu-preview-img");
+    const menuLinks = document.querySelectorAll(".link a")
+    let isMenuOpen = false;
+    let isAnimating = false;
+
+    menuToggle?.addEventListener("click", () => {
+      if (!isMenuOpen) openMenu();
+      else closeMenu();
+    });
+
+    function cleanupPreviewImages() {
+      const previewImages = menuPreviewImg?.querySelectorAll("img");
+
+      if (previewImages && previewImages.length > 3) {
+        for (let i = 0; i < previewImages.length - 3; i++) {
+          menuPreviewImg?.removeChild(previewImages[i]);
+        }
+      }
+    }
+
+    function resetPreviewImg() {
+      if (menuPreviewImg) {
+        menuPreviewImg.innerHTML = "";
+        const defaultPreviewImg = document.createElement("img");
+        defaultPreviewImg.src = "/img-1.jpg";
+        menuPreviewImg.appendChild(defaultPreviewImg);
+      }
+    }
+
+    function animateMenuToggle(isOpening: boolean) {
+      const open = document.querySelector("p#menu-open");
+      const close = document.querySelector("p#menu-close");
+
+      gsap.to(isOpening ? open : close, {
+        x: isOpening ? -5 : 5,
+        y: isOpening ? -10 : 10,
+        rotation: isOpening ? -5 : 5,
+        opacity: 0,
+        delay: 0.25,
+        duration: 0.5,
+        ease: "power2.out",
+      });
+
+      gsap.to(isOpening ? close : open, {
+        x: 0,
+        y: 0,
+        rotation: 0,
+        opacity: 1,
+        delay: 0.5,
+        duration: 0.5,
+        ease: "power2.out",
+      });
+    }
+
+    function openMenu() {
+      if (isAnimating || isMenuOpen) return;
+      isAnimating = true;
+
+      gsap.to(container, {
+        rotation: 10,
+        x: 300,
+        y: 450,
+        scale: 1.5,
+        duration: 1.25,
+        ease: "power4.inOut",
+      });
+
+      animateMenuToggle(true);
+
+      gsap.to(menuContent, {
+        rotation: 0,
+        x: 0,
+        y: 0,
+        opacity: 1,
+        scale: 1,
+        duration: 1.25,
+        ease: "power4.inOut",
+      });
+
+      gsap.to([".link a", ".social a"], {
+        y: "0%",
+        opacity: 1,
+        duration: 1,
+        delay: 0.75,
+        stagger: 0.1,
+        ease: "power3.inOut",
+      });
+
+      gsap.to(menuOverlay, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 175%, 0% 100%)",
+        duration: 1.25,
+        ease: "power4.inOut",
+        onComplete: () => {
+          isMenuOpen = true;
+          isAnimating = false;
+        },
+      });
+    }
+
+    function closeMenu() {
+      if (isAnimating || !isMenuOpen) return;
+      isAnimating = true;
+
+      gsap.to(container, {
+        rotation: 0,
+        x: 0,
+        y: 0,
+        scale: 1,
+        duration: 1.25,
+        ease: "power4.inOut",
+      });
+
+      animateMenuToggle(false);
+
+      gsap.to(menuContent, {
+        rotation: -15,
+        x: -100,
+        y: -100,
+        opacity: 0.25,
+        scale: 1.5,
+        duration: 1.25,
+        ease: "power4.inOut",
+      });
+
+      gsap.to([".link a", ".social a"], {
+        y: "100%",
+        opacity: 0,
+        duration: 1,
+        delay: 0.25,
+        stagger: 0.1,
+        ease: "power3.inOut",
+      });
+
+      gsap.to(menuOverlay, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        duration: 1.25,
+        ease: "power4.inOut",
+        onComplete: () => {
+          isMenuOpen = false;
+          isAnimating = false;
+          gsap.set([".link a", ".social, a"], { y: "120" });
+          resetPreviewImg();
+        },
+      });
+    }
+
+    menuLinks?.forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        if(!isMenuOpen || isAnimating) return;
+
+        const imgSrc = link.getAttribute("data-img")
+        if(!imgSrc) return;
+
+        const previewImages = menuPreviewImg?.querySelectorAll("img");
+        if(previewImages.length > 0 && previewImages[previewImages.length - 1].src.endsWith(imgSrc))
+          return;
+
+        const newPreviewImg = document.createElement("img")
+        newPreviewImg.src = imgSrc;
+        newPreviewImg.style.opacity = "0";
+        newPreviewImg.style.transform  = "scale(1.25) rotate(10deg)";
+
+        menuPreviewImg?.appendChild(newPreviewImg);
+        cleanupPreviewImages();
+
+        gsap.to(newPreviewImg, {
+          opacity: 1,
+          scale: 1,
+          rotation : 0,
+          duration: 0.75,
+          ease: "power2.out"
+
+        })
+      })
+    });
+  }, {});
   return (
     <>
       <nav>
@@ -8,7 +195,7 @@ export default function Home() {
 
         <div className="menu-toggle">
           <p id="menu-open">Menu</p>
-          <p id="menu-open">Close</p>
+          <p id="menu-close">Close</p>
         </div>
       </nav>
       {/* Clip path to the the menu-overlay and tilt animation to the menu-content*/}
@@ -17,7 +204,7 @@ export default function Home() {
           <div className="menu-items">
             <div className="col-lg">
               <div className="menu-preview-img">
-                <img src="/img-1.jpg" alt="" />
+                <Image src="/img-1.jpg" alt="" width={400} height={300} />
               </div>
             </div>
             <div className="col-sm">
@@ -36,7 +223,7 @@ export default function Home() {
 
                 <div className="link">
                   <a href="#" data-img="/img-3.jpg">
-                    Work
+                    Signals
                   </a>
                 </div>
 
@@ -71,15 +258,15 @@ export default function Home() {
                   </a>
                 </div>
               </div>
-              <div className="menu-footer">
-                <div className="col-lg">
-                  <a href="#">Run Sequence</a>
-                </div>
-                <div className="col-sm">
-                  <a href="#">Origin</a>
-                  <a href="#">Know More</a>
-                </div>
-              </div>
+            </div>
+          </div>
+          <div className="menu-footer">
+            <div className="col-lg">
+              <a href="#">Run Sequence</a>
+            </div>
+            <div className="col-sm">
+              <a href="#">Origin</a>
+              <a href="#">Join Signal</a>
             </div>
           </div>
         </div>
@@ -88,7 +275,7 @@ export default function Home() {
       <div className="container">
         <section className="hero">
           <div className="hero-img">
-            <img src="/hero.jpg" alt="" />
+            <Image src="/hero.jpg" alt="" width={800} height={600} />
           </div>
           <h1>Elevate your websites with Intention.</h1>
         </section>
